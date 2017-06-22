5ca0abf2cf8277815de1d873e8e6078e963881a5
Jon Dugan <jdugan@x1024.net>
2017-06-22 16:33:40 -0500

    use logging instead of print

--- shibboleth_session_auth/views.py
+++ shibboleth_session_auth/views.py
@@ -1,9 +1,13 @@
+import logging
+
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseForbidden, \
                         HttpResponseBadRequest, HttpResponseRedirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User, Group
 from django.core.urlresolvers import get_script_prefix
+
+logger = logging.getLogger(__name__)
 
 def shibboleth_session_auth(request):
     """Authenticate a session using Shibboleth.
@@ -18,13 +22,11 @@ def shibboleth_session_auth(request):
     idp_attr = settings.SHIBBOLETH_SESSION_AUTH['IDP_ATTRIBUTE']
     idp = request.META.get(idp_attr)
     if not idp:
-        # log
-        print "IdP header missing ({})".format(idp_attr)
+        logger.error("IdP header missing (%s)", idp_attr)
         return HttpResponseBadRequest("Invalid response from IdP")
 
     if idp not in settings.SHIBBOLETH_SESSION_AUTH['AUTHORIZED_IDPS']:
-        # log
-        print "Unauthorized IdP: {}".format(idp)
+        logger.info("Unauthorized IdP: %s", idp)
         return HttpResponseForbidden("unauthorized IdP: {}".format(idp))
 
     user_attrs = {}
@@ -32,8 +34,7 @@ def shibboleth_session_auth(request):
     for http_attr, user_attr, required in settings.SHIBBOLETH_SESSION_AUTH['USER_ATTRIBUTES']:
         user_attrs[user_attr] = request.META.get(http_attr, None)
         if required and user_attrs[user_attr] is None:
-            # log
-            print "SSO missing attribute: {}".format(user_attr)
+            logger.error("SSO missing attribute: %s", user_attr)
             return HttpResponseBadRequest("Invalid response from IdP")
 
     try:
@@ -48,28 +49,24 @@ def shibboleth_session_auth(request):
             try:
                 group = Group.objects.get(name=group_name)
             except Group.DoesNotExist:
-                # log
-                print "creating group {} (locally configured for IdP {})".format(group_name, idp)
+                logger.info("creating group %s (locally configured for IdP %s)", group_name, idp)
                 continue
 
             if group not in user.groups.all():
                 user.groups.add(group)
-                print "adding user {} to group {}".format(user.username, group.name)
+                logger.info("adding user %s to group %s", user.username, group.name)
 
     group_attr = settings.SHIBBOLETH_SESSION_AUTH['GROUP_ATTRIBUTE']
     if group_attr in request.META:
         for group_name in request.META[group_attr].split(";"):
             group, created = Group.objects.get_or_create(name=group_name)
             if created:
-                #log
-                print "creating group {} (remotely provided by IdP {})".format(group_name, idp)
+                logging.info("creating group %s (remotely provided by IdP %s)", group_name, idp)
 
             if group not in user.groups.all():
                 group.user_set.add(user)
-                # log
-                print "adding user {} to group {}".format(user.username, group.name)
+                logger.info("adding user %s to group %s", user.username, group.name)
 
-    # see https://github.com/esnet/esnet-esdb/issues/13 for further discussion
     user = authenticate(remote_user=user.username)
     login(request, user)
 
