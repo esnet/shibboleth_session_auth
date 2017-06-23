f7ac639624aa0572c169d18abd9b0cb2be302792
Jon Dugan <jdugan@x1024.net>
2017-06-23 16:00:50 -0500

    0.6.0 release

--- setup.py
+++ setup.py
@@ -5,8 +5,13 @@ import sys
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
+try:
+    # Use pandoc to convert .md -> .rst when uploading to pypi
+    import pypandoc
+    DESCRIPTION = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError, OSError):
+    with open(path.join(here, "README.md"), encoding="utf-8") as f:
+        DESCRIPTION = f.read()
 
 
 if sys.version_info[0] == 2 and sys.version_info[1] < 7:
@@ -16,12 +21,12 @@ if sys.version_info[0] == 3:
     sys.exit('Sorry, Python 3 is not supported')
 
 setup(
-    name="shibboleth_session_auth",
+    name="django-shibboleth-session-auth",
 
-    version="0.6",
+    version="0.6.0",
 
     description="Simplistic Shibboleth integration for Django sessions",
-    long_description=long_description,
+    long_description=DESCRIPTION,
     url="https://github.com/esnet/shibboleth_session_auth",
 
     author="Jon M. Dugan",
